24a0fc8207c3fd32cadf0f1118830bef18fd06f3
VovaLyapota <rimidalvatopayl@gmail.com>
2024-03-25 16:38:32 +0200

    remove header from login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,4 @@
 import Button from '../../components/Button';
-import Header from '../../components/Header/Header';
 import LoginForm from '../../components/LoginForm/LoginForm';
 import Container from '../../components/Container';
 import NetworkButtons from '../../components/NetworkButtons';
@@ -9,27 +8,21 @@ import styles from './LoginPage.module.css';
 
 const LoginPage = () => {
   return (
-    <>
-      <Header />
-      <Container>
-        <div className={styles.formContainer}>
-          <PageTitle>Log in</PageTitle>
-          <LoginForm />
-          <Button type="button" variant="link">
-            Forgot password
-          </Button>
-          <OverlinedText>or log in with</OverlinedText>
-          <NetworkButtons
-            googleCallback={() => {}}
-            linkedinCallback={() => {}}
-          />
+    <Container>
+      <div className={styles.formContainer}>
+        <PageTitle>Log in</PageTitle>
+        <LoginForm />
+        <Button type="button" variant="link">
+          Forgot password
+        </Button>
+        <OverlinedText>or log in with</OverlinedText>
+        <NetworkButtons googleCallback={() => {}} linkedinCallback={() => {}} />
 
-          <p className={styles.text}>
-            Don't have an account? <Button variant="link">SIGN UP</Button>
-          </p>
-        </div>
-      </Container>
-    </>
+        <p className={styles.text}>
+          Don't have an account? <Button variant="link">SIGN UP</Button>
+        </p>
+      </div>
+    </Container>
   );
 };
 
